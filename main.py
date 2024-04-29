22216a92595ce10deee2a9e2bc95cf2dd614daf5
MaWi-BG <mwittich@bollinger-grohmann.de>
2024-04-29 14:53:00 +0200

    test

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 Use the automation_context module to wrap your function in an Autamate context helper
 """
+from specklepy.objects.geometry import Mesh
+from trimesh.primitives import Sphere as trimesh_sphere
 
 from pydantic import Field, SecretStr
 from speckle_automate import (
@@ -12,6 +14,37 @@ from speckle_automate import (
 
 from flatten import flatten_base
 
+class Sphere():
+
+    '''
+    A general placeholder for methods pertaining to a sphere object.
+    '''
+
+    def create(radius: float  = 1,
+               center: list = [0, 0, 0]):
+            
+        '''
+        Appropriately formatted sphere mesh for speckle_py.
+
+        Args:
+            radius (float): Radius of sphere
+            center (list): Center of sphere [x, y, z]
+
+        Returns:
+            speckle.objects.geometry.Mesh: A speckle mesh object.
+        '''
+
+        sphere = trimesh_sphere(radius = radius, center = center)
+
+        vertices = [item for sublist in sphere.vertices for item in sublist]
+
+        faces = []
+        for face in sphere.faces:
+            faces.append(3)
+            for vertex in face:
+                faces.append(int(vertex))
+
+        return Mesh.create(vertices, faces)
 
 class FunctionInputs(AutomateBase):
     """These are function author defined values.
