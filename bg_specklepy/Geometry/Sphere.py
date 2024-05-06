9d18c393e30323423e455aceb877ec59c20415a5
MaWi-BG <mwittich@bollinger-grohmann.de>
2024-05-06 15:26:59 +0200

    Test

---
+++ bg_specklepy/Geometry/Sphere.py
@@ -0,0 +1,31 @@
+class Sphere():
+
+    '''
+    A general placeholder for methods pertaining to a sphere object.
+    '''
+
+    def create(radius: float  = 1,
+               center: list = [0, 0, 0]):
+            
+        '''
+        Appropriately formatted sphere mesh for speckle_py.
+
+        Args:
+            radius (float): Radius of sphere
+            center (list): Center of sphere [x, y, z]
+
+        Returns:
+            speckle.objects.geometry.Mesh: A speckle mesh object.
+        '''
+
+        sphere = trimesh_sphere(radius = radius, center = center)
+
+        vertices = [item for sublist in sphere.vertices for item in sublist]
+
+        faces = []
+        for face in sphere.faces:
+            faces.append(3)
+            for vertex in face:
+                faces.append(int(vertex))
+
+        return Mesh.create(vertices, faces) 
